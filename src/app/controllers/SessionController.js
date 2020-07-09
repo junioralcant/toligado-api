e58fe6cacc4131eaa014f8b14a143d9554791dd2
junioralcant <junioralcant111@gmail.com>
2020-07-09 09:47:11 -0300

    Login with cpf

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,15 +2,11 @@ const User = require("../models/User");
 
 class SessionController {
   async store(req, resp) {
-    const { cpf, password } = req.body;
+    const { cpf } = req.body;
 
     const user = await User.findOne({ cpf });
     if (!user) {
       return resp.status(400).json({ error: "Usuario não encontrado" });
-    }
-
-    if (!(await user.compareHash(password))) {
-      return resp.status(400).json({ error: "Senha inválida" });
     }
 
     return resp.json({ user, token: User.generateToken(user) });
