6ec5f373f48ad8cc1e63f06919920f4b3676222f
junioralcant <junioralcant111@gmail.com>
2022-08-06 10:40:18 -0300

    Add filter date

--- src/app/controllers/DangerRecordController.js
+++ src/app/controllers/DangerRecordController.js
@@ -1,10 +1,14 @@
 const DangerRecord = require('../models/DangerRecord');
 const Image = require('../models/Image');
 const User = require('../models/User');
-
+const moment = require('moment');
 class DangerRecordController {
   async index(req, res) {
     const userLogged = await User.findById(req.userId);
+
+    const { initialDate, finalDate } = req.query;
+
+    const filters = {};
 
     if (userLogged) {
       const dangers = await DangerRecord.find({
@@ -17,13 +21,42 @@ class DangerRecordController {
       return res.json(dangers);
     }
 
-    const dangers = await DangerRecord.paginate(null, {
-      limit: 100,
-      populate: ['image', 'user', 'imageResolved'],
+    if (initialDate && finalDate) {
+      filters.createdAt = {};
+
+      const initial = moment(initialDate).format(
+        'YYYY-MM-DDT00:mm:ss.SSSZ'
+      );
+
+      const final = moment(finalDate).format(
+        'YYYY-MM-DDT23:59:ss.SSSZ'
+      );
+
+      filters.createdAt.$gte = initial;
+      filters.createdAt.$lte = final;
+    }
+
+    let dangers = await DangerRecord.paginate(filters, {
+      limit: 1000000000,
+      populate: ['image', 'imageResolved', 'user'],
+
       sort: '-createdAt',
     });
 
-    return res.json(dangers);
+    // Filtra por dados do mes e ano atual
+    if (!initialDate || !finalDate) {
+      dangers = dangers.docs.filter(
+        (danger) =>
+          moment(danger.createdAt).month() ===
+            moment(Date.now()).month() &&
+          moment(danger.createdAt).year() ===
+            moment(Date.now()).year()
+      );
+
+      return res.json(dangers);
+    }
+
+    return res.json(dangers.docs);
   }
 
   async store(req, res) {
